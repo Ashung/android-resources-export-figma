5823305359d0374e85972553318c136f6cc6e44b
Ashung Hung <ashung.hung@gmail.com>
2022-01-13 19:17:00 +0800

    update node modules.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,49 @@
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = (env, argv) => ({
-    // This is necessary because Figma's 'eval' works differently than normal eval
+    mode: argv.mode === 'production' ? 'production' : 'development',
+
     devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
     entry: {
         ui: './src/ui.ts',
         code: './src/code.ts'
     },
+
     module: {
         rules: [
-            // Converts TypeScript code to JavaScript
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            // Enables including CSS by doing "import './file.css'" in your TypeScript code
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            }
+            { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+            { test: /\.css$/, use: ['style-loader', { loader: 'css-loader' }] },
         ]
     },
+
     resolve: {
         extensions: [ '.tsx', '.ts', '.js' ]
     },
+
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
     },
 
-    // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            })
+        ],
+    },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/ui.html',
@@ -41,6 +51,6 @@ module.exports = (env, argv) => ({
             inlineSource: '.(js)$',
             chunks: ['ui'],
         }),
-        new HtmlWebpackInlineSourcePlugin(),
+        new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
     ],
 });
